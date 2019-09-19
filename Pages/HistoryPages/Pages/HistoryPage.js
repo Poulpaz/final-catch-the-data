65e5f49f1b7a45696dc18455e9d964e16d3a3cc5
Poulpaz <tristan.reffay@gmx.fr>
2019-09-19 12:00:46 +0200

    history design ended

--- Pages/HistoryPages/Pages/HistoryPage.js
+++ Pages/HistoryPages/Pages/HistoryPage.js
@@ -1,12 +1,68 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  ScrollView,
+  View,
+  Alert,
+  RefreshControl,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
+import { Appbar, Button, List, Card } from "react-native-paper";
+
+import styles from "../Styles/HistoryPageStyles";
 
 export default class HistoryPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      activities: []
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  componentWillUnmount() {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>History Page</Text>
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <Appbar.Header>
+          <Appbar.Content
+            title="Historique"
+            subtitle="Voir ses records et moyennes"
+          />
+          <Appbar.Action
+            icon="apps"
+            onPress={() => console.log("GO TO ALL HISTORY")}
+          />
+        </Appbar.Header>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={console.log("REFRESHING")}
+            />
+          }
+        >
+          <Card style={styles.historyListCard}>
+            <List.Section>
+              <List.Item title="Aucune activité enregistrée." />
+            </List.Section>
+          </Card>
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }

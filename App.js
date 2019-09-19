779c71036cc16bddf7d85e708dc85683c35244f5
Poulpaz <tristan.reffay@gmx.fr>
2019-09-19 11:47:30 +0200

    theme provider ended

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import Navigator from "./Navigation/Navigator";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
 export default class App extends React.Component {
   render() {
-    return <Navigator />;
+    return (
+      <PaperProvider theme={theme}>
+        <Navigator />
+      </PaperProvider>
+    );
   }
 }
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#013f99",
+    secondary: "#d93f2b",
+    accent: "#f1c40f",
+    background: "#ffffff"
+  }
+};

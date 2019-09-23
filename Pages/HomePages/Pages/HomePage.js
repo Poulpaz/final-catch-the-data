7274dd9ec24e6fee6d02cd1b916e90c4e13790c0
Poulpaz <tristan.reffay@gmx.fr>
2019-09-23 16:13:41 +0200

    save and load data home settings ended

--- Pages/HomePages/Pages/HomePage.js
+++ Pages/HomePages/Pages/HomePage.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { View, Picker, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import {
+  View,
+  Picker,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  ToastAndroid
+} from "react-native";
 
 import { Appbar, Button, Card, TextInput } from "react-native-paper";
 
 import homeStyles from "../Styles/HomePageStyles";
 
+import HomeDataModel from "../../../Storage/HomeDataModel";
+
+const homeData = {
+  columns: "id, position, roadState, temperature",
+  page: 1,
+  limit: 1,
+  order: "id DESC"
+};
+
 export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      settings: [],
+      idHomeSettings: null,
       position: 0.0,
       roadState: 0.0,
       temperature: 0
@@ -16,12 +34,55 @@ export default class HomePage extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.state);
+    this._loadHomeSettings();
   }
 
   componentDidUpdate() {
-    console.log(this.state);
+    //console.log(this.state);
   }
+
+  _loadHomeSettings = () => {
+    return HomeDataModel.query(homeData)
+      .then(res => {
+        this.setState({
+          settings: res
+        }),
+          this.state.settings.map(item =>
+            this.setState({
+              idHomeSettings: item.id,
+              position: item.position,
+              roadState: item.roadState,
+              temperature: item.temperature
+            })
+          ),
+          console.log(this.state);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  _saveHomeSettings = () => {
+    if (this.state.settings && this.state.settings.length > 0) {
+      const homeDataEdit = {
+        id: this.state.idHomeSettings,
+        position: parseFloat(this.state.position).toFixed(2),
+        roadState: parseFloat(this.state.roadState).toFixed(4),
+        temperature: parseInt(this.state.temperature)
+      };
+      HomeDataModel.update(homeDataEdit).then(this._loadHomeSettings());
+    } else {
+      const homeDataNew = {
+        position: parseFloat(this.state.position).toFixed(2),
+        roadState: parseFloat(this.state.roadState).toFixed(4),
+        temperature: parseInt(this.state.temperature)
+      };
+      HomeDataModel.create(homeDataNew)
+        .then(res => this.setState({ idHomeSettings: res.id }))
+        .then(this._loadHomeSettings());
+    }
+    ToastAndroid.show("Données enregistrées avec succès", ToastAndroid.SHORT);
+  };
 
   render() {
     return (
@@ -40,7 +101,7 @@ export default class HomePage extends React.Component {
             />
             <Card.Content>
               <Picker
-                selectedValue={this.state.position}
+                selectedValue={String(this.state.position)}
                 onValueChange={(itemValue, itemIndex) =>
                   this.setState({
                     position: itemValue
@@ -64,7 +125,7 @@ export default class HomePage extends React.Component {
             />
             <Card.Content>
               <Picker
-                selectedValue={this.state.roadState}
+                selectedValue={String(this.state.roadState)}
                 onValueChange={(itemValue, itemIndex) =>
                   this.setState({
                     roadState: itemValue
@@ -97,7 +158,7 @@ export default class HomePage extends React.Component {
             <Button
               icon="check"
               mode="contained"
-              onPress={() => console.log("PRESSED !")}
+              onPress={() => this._saveHomeSettings()}
             >
               Sauvegarder
             </Button>
